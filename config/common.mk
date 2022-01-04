2e13311b652e46143b0c14a90b557fc0625efe26
Maitreya29 <maitreyapatni30@gmail.com>
2022-01-04 18:50:58 +0530

    radiant: include fonts
    Signed-off-by: Maitreya29 <maitreyapatni30@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -170,3 +170,5 @@ include vendor/radiant/config/packages.mk
 # Enable support of one-handed mode
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.support_one_handed_mode=true
+
+include vendor/fonts/fonts.mk
