5bdd658669fb3a6c103337b7a08eb760610226ba
Maitreya29 <maitreyapatni30@gmail.com>
2022-01-14 17:51:21 +0530

    Fonts: Init

---
+++ fonts.mk
@@ -0,0 +1,13 @@
+PRODUCT_PACKAGES := \
+    HarmonyOS_Sans_Thin.ttf \
+    HarmonyOS_Sans_Thin_Italic.ttf \
+    HarmonyOS_Sans_Regular.ttf \
+    HarmonyOS_Sans_Regular_Italic.ttf \
+    HarmonyOS_Sans_Medium.ttf \
+    HarmonyOS_Sans_Medium_Italic.ttf \
+    HarmonyOS_Sans_Light.ttf \
+    HarmonyOS_Sans_Light_Italic.ttf \
+    HarmonyOS_Sans_Bold.ttf \
+    HarmonyOS_Sans_Bold_Italic.ttf \
+    HarmonyOS_Sans_Black.ttf \
+    HarmonyOS_Sans_Black_Italic.ttf \
