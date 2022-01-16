69f538e682ddedab90570e696d7441fd51475f77
Maitreya29 <maitreyapatni30@gmail.com>
2022-01-16 15:08:58 +0530

    Radiant: Drop duplicate declaration and shift to System properties

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -3,16 +3,13 @@ BUILD_NUMBER_CUSTOM := $(shell date -u +%H%M)
 BUILD_SIGNATURE_KEYS := release-keys
 
 BUILD_FINGERPRINT := $(PRODUCT_BRAND)/$(TARGET_DEVICE)/$(TARGET_DEVICE):$(PLATFORM_VERSION)/$(BUILD_ID)/$(BUILD_NUMBER_CUSTOM):$(TARGET_BUILD_VARIANT)/$(BUILD_SIGNATURE_KEYS)
-ADDITIONAL_BUILD_PROPERTIES += \
-    ro.build.fingerprint=$(BUILD_FINGERPRINT) 
 
 # ProjectRadiant System Version
-ADDITIONAL_BUILD_PROPERTIES += \
+ADDITIONAL_SYSTEM_PROPERTIES += \
   ro.radiant.version=$(RADIANT_DISPLAY_VERSION) \
   ro.radiant.build.status=$(RADIANT_BUILD_TYPE) \
   ro.modversion=$(RADIANT_MOD_VERSION) \
   ro.radiant.build.date=$(BUILD_DATE) \
   ro.radiant.buildtype=$(RADIANT_BUILD_TYPE) \
   ro.radiant.fingerprint=$(RADIANT_FINGERPRINT) \
-  ro.radiant.device=$(RADIANT_BUILD) \
   org.radiant.version=$(RADIANTVERSION)
